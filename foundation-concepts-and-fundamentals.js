8571a6b7d68a16dfd1a2e7694050fb3e4dcf074d
MNeipp <ReamusLQ@gmail.com>
2020-05-02 11:28:35 -0700

    Finished 'Last Digit to A'

--- foundation-concepts-and-fundamentals.js
+++ foundation-concepts-and-fundamentals.js
@@ -173,3 +173,21 @@ function fibonacci(num){
   }
   else return fibonacci(num  -2) + fibonacci(num  -1)
   }
+
+
+/* Implement a function that accepts two non-negative integers as arguments. Function lastDigitAtoB(a,
+b) should return the last digit of the first number (a) raised to an exponent of the second number (b).
+Examples: given (3, 4), you should return 1 (the last digit of 81: 3 * 3 * 3 * 3). Given (12, 5), return
+2 (the least significant digit of 248832, which is 12 * 12 * 12 * 12 * 12).
+ */
+
+function lastDigitAtoB(a,b){
+  var x = a
+  for(i = 1; i < b; i++){
+    if (a > 9){
+      a = a%10
+    }
+    a*=x
+  }
+  return (a%10);
+}
