cdd97628c32a50ba67e6d2285f11dfebb1e630c7
MNeipp <ReamusLQ@gmail.com>
2020-05-05 12:08:44 -0700

    Initial Commit "Chapter-2-arrays"

---
+++ chapter-2-arrays.js
@@ -0,0 +1,30 @@
+/* Given an array and an additional value, insert this
+value at the beginning of the array. Do this
+without using any built-in array methods.
+ */
+function pushFront(arr,num){
+    for(i = arr.length-1; i>=0; i--){
+        arr[i+1] = arr[i]
+    }
+    arr[0] = num
+    return arr;
+}
+
+/* Given an array, remove and return the value at
+the beginning of the array. Do this without using
+any built-in array methods except pop(). */
+
+function popFront(arr){
+    var front = arr[0]
+    for(i = 0; i<arr.length; i++){
+        arr[i] = arr[i+1]
+    }
+    arr.pop();
+    console.log(arr);
+    return front;
+}
+
+/* Given an array, index, and additional value, insert
+the value into the array at the given index. Do this
+without using built-in array methods */
+
