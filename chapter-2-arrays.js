0b0194ba23d48e7984b73f9abe903b512d54346c
MNeipp <ReamusLQ@gmail.com>
2020-05-06 14:59:10 -0700

    Finished through "Reverse Array"

--- chapter-2-arrays.js
+++ chapter-2-arrays.js
@@ -36,3 +36,37 @@ function insertAt(arr,index,num){
     return arr
 }
 console.log(insertAt([1,2,3,4,5],1,3))
+
+/* Given an array and an index into the array,
+remove and return the array value at that index.
+Do this without using any built-in array methods
+except pop(). */
+
+function removeAt(arr,indx){
+    var front = arr[indx]
+    for(i = arr[indx-1]; i<arr.length; i++){
+        arr[i] = arr[i+1]
+    }
+    arr.pop();
+    console.log(arr);
+    return front;
+}
+
+/* Given a numerical array, reverse the order of the
+values. The reversed array should have the same
+length, with existing elements moved to other
+indices so that the order of elements is reversed. */
+function reverseArray(arr){
+    var temp = 0
+    for(i = 0; i < Math.floor(arr.length/2); i++){
+        temp = arr[arr.length-i-1];
+        arr[arr.length-i-1] = arr[i];
+        arr[i] = temp;
+    }
+    return arr;
+}
+
+/* Implement a function removeNegatives() that
+accepts an array and removes any values that
+are less than zero.
+Second-level challenge: don’t use nested loops. */
