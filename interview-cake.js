0e821c9314cc05667c0b12e2c3ba7b548026403e
MNeipp <ReamusLQ@gmail.com>
2020-09-17 09:06:01 -0600

    merge arrays question

---
+++ interview-cake.js
@@ -0,0 +1,29 @@
+// We have our lists of orders sorted numerically already, in arrays. Write a function to merge our arrays of orders into one sorted array.
+
+function mergeArrays(arr1, arr2){
+    let newArr = [];
+    let arr1Index = 0;
+    let arr2Index = 0;
+    let combinedLength = (arr1.length + arr2.length)
+
+    for(let i = 0; i < combinedLength; i++){
+        if( arr1[arr1Index] === undefined && arr2[arr2Index] === undefined){
+            return newArr
+        }
+        else if (arr1[arr1Index] === undefined){
+            newArr.push(arr2[arr2Index])
+            arr2Index++
+        } else if (arr2[arr2Index] === undefined){
+            newArr.push(arr1[arr1Index])
+            arr1Index++
+        }
+        else if (arr1[arr1Index] < arr2[arr2Index]){
+            newArr.push(arr1[arr1Index])
+            arr1Index++
+        } else {
+            newArr.push(arr2[arr2Index])
+            arr2Index++
+        }
+    }
+    return newArr
+}
